500160718b7ceb244f43833e92dd09b3e686f5b4
amwetgwxf <79970196+amwetgwxf@users.noreply.github.com>
2023-07-25 09:28:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,27 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/10.27.8.26/g' package/base-files/files/bin/config_generate
+
+# 修改网关
+sed -i 's/192.168.$((addr_offset++)).1/10.27.$((addr_offset++)).26/g' package/base-files/files/bin/config_generate
+
+# Modify default Hostname 修改主机名字，把MI-R3G修改你喜欢的就行（不能使用中文）
+sed -i 's/ImmortalWrt/openwrt-26/g' package/base-files/files/bin/config_generate
+
+# 清除登陆密码
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/default-settings/files/zzz-default-settings
+#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
+
+# 修改wifi名称
+#sed -i 's/OpenWrt/MI/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 修改 edge 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
+#sed -i 's/luci-theme-bootstrap/luci-theme-edge/g' openwrt/feeds/luci/collections/luci/Makefile
+
+#Enable 802.11k/v/r
+sed -i 's/RRMEnable=0/RRMEnable=1/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.1.2G.dat
+sed -i 's/RRMEnable=0/RRMEnable=1/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.1.5G.dat
+sed -i 's/FtSupport=0/FtSupport=1/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.1.2G.dat
+sed -i 's/FtSupport=0/FtSupport=1/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.1.5G.dat
+echo 'WNMEnable=1' >> package/kernel/mt-drivers/mt_wifi/files/mt7615.1.2G.dat
+echo 'WNMEnable=1' >> package/kernel/mt-drivers/mt_wifi/files/mt7615.1.5G.dat
